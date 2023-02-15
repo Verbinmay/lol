a2acd5992fe6c49ee51024eef16ca56f24d7f6ed
Mark_Maistrenko <revanmayfall@gmail.com>
2023-02-16 00:45:02 +0400

    Start

---
+++ src/application/jwt-service.ts
@@ -0,0 +1,21 @@
+import { ObjectId } from "mongodb";
+import { setting } from "../settings";
+import jwt from "jsonwebtoken";
+import { UserDBModel } from "../types";
+export const jwtService = {
+  async createJWT(user: UserDBModel) {
+    const token = jwt.sign({ userId: user._id }, setting.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    return token;
+  }
+  ,
+  async getUserIdByToken(token:string) {
+    try {
+      const result:any = jwt.verify(token, setting.JWT_SECRET);
+      return new ObjectId(result.userId);
+    } catch (error) {
+      return null;
+    }
+  },
+};
---
+++ src/types.ts
@@ -0,0 +1,88 @@
+import { ObjectId } from "mongodb";
+
+export type APIErrorResult = Array<FieldError>;
+
+export type FieldError = {
+  message: string;
+  field: string;
+};
+
+export type Paginator = {
+  pagesCount: number;
+  page: number;
+  pageSize: number;
+  totalCount: number;
+};
+
+export type PaginatorBlog = Paginator & {
+  items: Array<BlogViewModel>;
+};
+
+export type PaginatorPost = Paginator & {
+  items: Array<PostViewModel>;
+};
+
+export type PaginatorUser = Paginator & {
+  items: Array<UserViewModel>;
+};
+
+export type BlogInputModel = {
+  name: string;
+  description: string;
+  websiteUrl: string;
+};
+
+export type BlogPostInputModel = {
+  title: string;
+  shortDescription: string;
+  content: string;
+};
+
+export type PostInputModel = {
+  title: string;
+  shortDescription: string;
+  content: string;
+  blogId: string;
+};
+
+export type BlogViewModel = {
+  id: string;
+  name: string;
+  description: string;
+  websiteUrl: string;
+  createdAt: string;
+  isMembership: boolean;
+};
+
+export type PostViewModel = {
+  id: string;
+  title: string;
+  shortDescription: string;
+  content: string;
+  blogId: string;
+  blogName: string;
+  createdAt: string;
+};
+
+export type LoginInputModel = {
+  loginOrEmail: string;
+  password: string;
+};
+
+export type UserInputModel = {
+  login: string;
+  password: string;
+  email: string;
+};
+
+export type UserViewModel = {
+  id: string;
+  login: string;
+  email: string;
+  createdAt: string;
+};
+
+export type UserDBModel= UserViewModel &  {
+  _id:any,
+  hash:string
+};
